3486aafbd16ada32e0ef8ff62eb8d44608d30482
juliodebuen <julio_halo10@hotmail.com>
2016-07-24 14:19:20 -0500

    revisarSol

---
+++ RevisarSolicitud.php
@@ -0,0 +1,29 @@
+<?php 
+    if(!empty($_POST['id'])){
+        mysql_connect('localhost','root','root')
+        or die("Error al conectar " . mysql_error());
+        mysql_select_db('analisis') //aca era proyecto
+        or die ("Error al seleccionar la Base de datos: " . mysql_error());
+        
+        $informacion = mysql_query("SELECT * FROM solicitudes WHERE id_solicitud=".$_POST['id']);
+        $pdf = mysql_query("SELECT * FROM scan WHERE id_solicitud=".$_POST['id']);
+        $row = mysql_fetch_array($informacion);
+    }        
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+        <p>        
+            Nombre(s): <?php echo $row[1]; ?> <br>
+            Apellidos: <?php echo $row[2]; ?> <br>
+            Direccion: <?php echo $row[3]; ?> <br>
+            E-mail   : <?php echo $row[4]; ?> <br>
+            Fecha de nacimiento: <?php echo $row[5]; ?> <br>
+            Lugar de residencia: <?php echo $row[6]; ?> <br>
+            Telefono : <?php echo $row[7]; ?> <br>
+        </p>
+        
+    </body>
+</html>
--- registro.php
+++ registro.php
@@ -20,8 +20,8 @@
         or die ("Error al seleccionar la Base de datos: " . mysql_error());
 
             $permitidos = array(".pdf");
-            $limite_kb = 16384;
-            if (in_array($_FILES['archivo']['type'], $permitidos) && $_FILES['archivo']['size'] <= $limite_kb * 1024){                
+            $limite_kb = 16384000;
+        //if (in_array($_FILES['archivo']['type'], $permitidos) && $_FILES['archivo']['size'] <= $limite_kb * 1024){                
 
             $sql = "INSERT INTO solicitudes(nombre,apellidos,direccion,correo,fecha,lugar,telefono) VALUES ('$name','$lastname','$address','$email','$birthday','$place','$phone')";//Se insertan los datos a la base de datos y el usuario ya fue registrado con exito.
             mysql_query($sql);
@@ -35,9 +35,10 @@
                     fclose($fp);
                     //escapar los caracteres
                     $data = mysql_escape_string($data);
-            $link = mysql_connect('localhost', 'root','root'); 
-            mysql_select_db('solicitudes', $link); 
-            $result = mysql_query("SELECT id_solicitud FROM solicitudes WHERE nombre = '$name' AND correo = '$email' AND telefono = '$phone'", $link);
+            /*$link = mysql_connect('localhost', 'root','root'); 
+            mysql_select_db('solicitudes', $link); */
+            echo $data;
+            $result = mysql_query("SELECT id_solicitud FROM solicitudes WHERE nombre = '$name' AND correo = '$email' AND telefono = '$phone'");
             $resultado = mysql_query("INSERT INTO scan (id_solicitud, archivo, tipo_imagen) VALUES ('$result', $data', '$tipo')") ;
 
             if ($resultado){                
@@ -45,9 +46,9 @@
             } else {
                 echo '<script language="javascript">alert("ocurrio un error al copiar el archivo.");</script> ';
             }
-        } else {
+        /*} else {
             echo '<script language="javascript">alert("archivo no permitido, es tipo de archivo prohibido o excede el tamaño de $limite_kb Kilobytes");</script> ';
-        }
+        }*/
                 echo "<script>location.href='registro.html'</script>";
         }
 ?>
