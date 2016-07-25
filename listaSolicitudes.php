9081ef7551ddd2da27086926f9c92858549d1a0c
juliodebuen <julio_halo10@hotmail.com>
2016-07-24 19:17:02 -0500

    update

--- listaSolicitudes.php
+++ listaSolicitudes.php
@@ -1,8 +1,8 @@
 <?php
 
   //Conexion a BD
-  $conexion = mysql_connect('', 'root', '')  or die('No se pudo conectar: ' . mysql_error());
-  echo "Conexion Exitosa ";
+  $conexion = mysql_connect('localhost', 'root', 'root')  or die('No se pudo conectar: ' . mysql_error());
+  //echo "Conexion Exitosa ";
 
   //SELECCIONANDO DB
   mysql_select_db('analisis') or die('No se pudo seleccionar la base de datos');
@@ -28,7 +28,7 @@
       <tr>
         <td>
           <?php echo $row[0]," ",$row[1]," ",$row[2],$row[3]," ",$row[4],$row[5]," ",$row[6]," ",$row[7]?>
-          <form class="" action="id.php" method="post">
+          <form class="" action="RevisarSolicitud.php" method="post">
             <input type="hidden" name="id" value="<?php echo $row[0] ?>">
             <button type="submit" name="button">Mas</button>
           </form>
